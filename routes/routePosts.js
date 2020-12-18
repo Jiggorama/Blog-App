58172d810c2e6b513e4a9c9f6982a3a3e50da226
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2020-12-18 16:48:21 -0500

    backend good

--- routes/routePosts.js
+++ routes/routePosts.js
@@ -1,1 +1,11 @@
-'test'
+const { Router } = require('express')
+const con = require('../controllers/conPosts')
+const router = Router()
+
+router.get('/posts', con.getPosts)
+router.get('/posts/:id', con.getPost)
+router.post('/posts', con.createPost)
+router.put('/posts/:id', con.updatePost)
+router.delete('/posts/:id', con.deletePost)
+
+module.exports = router
