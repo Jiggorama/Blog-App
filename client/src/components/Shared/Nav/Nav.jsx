4b3747f4ade1be7d45765d68cd72430b63d23098
David Verghese <dj.verghese@gmail.com>
2020-12-22 12:07:40 -0500

    worked on CSS with Amy

--- client/src/components/Shared/Nav/Nav.jsx
+++ client/src/components/Shared/Nav/Nav.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import './Nav.css'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav>
-      <div className='nav'>
-        <NavLink className='logo' to='/'>Blog-App</NavLink>
-        <div className='links'>
-          <NavLink className='link' to='/posts'>Posts</NavLink>
-          <NavLink className='link' to='/post-create'>Create Post</NavLink>
-        </div>         
+      <div className="nav">
+        <NavLink className="logo" to="/">
+          Blog-App
+        </NavLink>
+        {/* <div className='links'> */}
+        <NavLink className="link1" to="/posts">
+          Posts
+        </NavLink>
+        <NavLink className="link2" to="/post-create">
+          Create Post
+        </NavLink>
+        {/* </div>          */}
       </div>
     </nav>
   );
--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./PostCreate.css";
 import Layout from "../../components/Shared/Layout/Layout";
-import { Redirect } from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 import { createPost } from "../../services/apiPosts.js";
 
 const PostCreate = (props) => {
@@ -32,47 +32,49 @@ const PostCreate = (props) => {
     return <Redirect to={`/posts`} />;
   }
   return (
-    <Layout user={props.user}>
-      <form className="create-form" onSubmit={handleSubmit}>
-        <input
-          className="input-title"
-          placeholder="Title"
-          value={post.title}
-          name="title"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-author"
-          placeholder="Author"
-          value={post.author}
-          name="author"
-          required
-          onChange={handleChange}
-        />
-        <textarea
-          className="textarea-content"
-          rows={10}
-          placeholder="Content"
-          value={post.content}
-          name="content"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-image-link"
-          placeholder="Image Link"
-          value={post.imgURL}
-          name="imgURL"
-          required
-          onChange={handleChange}
-        />
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-    </Layout>
+    <div className="form-background">
+      <Layout user={props.user}>
+        <form className="create-form" onSubmit={handleSubmit}>
+          <input
+            className="input-title"
+            placeholder="Title"
+            value={post.title}
+            name="title"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-author"
+            placeholder="Author"
+            value={post.author}
+            name="author"
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            className="textarea-content"
+            rows={10}
+            placeholder="Content"
+            value={post.content}
+            name="content"
+            required
+            onChange={handleChange}
+          />
+          <input
+            className="input-image-link"
+            placeholder="Image Link"
+            value={post.imgURL}
+            name="imgURL"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </Layout>
+    </div>
   );
 };
 
